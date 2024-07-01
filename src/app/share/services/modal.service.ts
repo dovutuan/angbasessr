703d95255c5eb2893825734e8a291edebc7bbdcc
tuan.do1 <tuan.do1@ntq-solution.com.vn>
2024-07-01 14:18:22 +0700

    chore(update modal)

--- src/app/share/services/modal.service.ts
+++ src/app/share/services/modal.service.ts
@@ -6,87 +6,87 @@ import {NzSafeAny} from 'ng-zorro-antd/core/types';
 import {TypeModalEnum} from '../core/enums/modal.enum';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class ModalService {
 
-    constructor(
-        private nzModalService: NzModalService
-    ) {
-    }
+  constructor(
+    private nzModalService: NzModalService
+  ) {
+  }
 
-    onConfirm = (title?: string | TemplateRef<{}>,
+  ngOnConfirm = (title?: string | TemplateRef<{}>,
                  content?: string | TemplateRef<NzSafeAny> | Type<NzSafeAny>,
                  onOk?: any,
                  onCancel?: any): void => {
-        this.onBuildModal(TypeModalEnum.CONFIRM, title, content, onOk, onCancel);
-    };
+    this.ngOnBuildModal(TypeModalEnum.CONFIRM, title, content, onOk, onCancel);
+  };
 
-    onInfo = (title?: string | TemplateRef<{}>,
+  ngOnInfo = (title?: string | TemplateRef<{}>,
               content?: string | TemplateRef<NzSafeAny> | Type<NzSafeAny>,
               onOk?: any,
               onCancel?: any): void => {
-        this.onBuildModal(TypeModalEnum.INFO, title, content, onOk, onCancel);
-    };
+    this.ngOnBuildModal(TypeModalEnum.INFO, title, content, onOk, onCancel);
+  };
 
-    onSuccess = (title?: string | TemplateRef<{}>,
+  ngOnSuccess = (title?: string | TemplateRef<{}>,
                  content?: string | TemplateRef<NzSafeAny> | Type<NzSafeAny>,
                  onOk?: any,
                  onCancel?: any): void => {
-        this.onBuildModal(TypeModalEnum.SUCCESS, title, content, onOk, onCancel, true);
-    };
+    this.ngOnBuildModal(TypeModalEnum.SUCCESS, title, content, onOk, onCancel, true);
+  };
 
-    onError = (title?: string | TemplateRef<{}>,
+  ngOnError = (title?: string | TemplateRef<{}>,
                content?: string | TemplateRef<NzSafeAny> | Type<NzSafeAny>,
                onOk?: any,
                onCancel?: any): void => {
-        this.onBuildModal(TypeModalEnum.ERROR, title, content, onOk, onCancel);
-    };
+    this.ngOnBuildModal(TypeModalEnum.ERROR, title, content, onOk, onCancel);
+  };
 
-    onWarning = (title?: string | TemplateRef<{}>,
+  ngOnWarning = (title?: string | TemplateRef<{}>,
                  content?: string | TemplateRef<NzSafeAny> | Type<NzSafeAny>,
                  onOk?: any,
                  onCancel?: any): void => {
-        this.onBuildModal(TypeModalEnum.WARNING, title, content, onOk, onCancel);
-    };
+    this.ngOnBuildModal(TypeModalEnum.WARNING, title, content, onOk, onCancel);
+  };
 
-    private onBuildModal = (type: string,
+  private ngOnBuildModal = (type: string,
                             title?: string | TemplateRef<{}>,
                             content?: string | TemplateRef<NzSafeAny> | Type<NzSafeAny>,
                             onOk?: any,
                             onCancel?: any,
                             isAutoClose: boolean = false,
                             timeAutoClose: number = 11500) => {
-        let modal = null;
+    let modal = null;
 
-        const options: ModalInterface = ModalConfirmConfig;
-        options.nzTitle = title;
-        options.nzContent = content;
-        onOk && (options.nzOnOk = onOk);
-        onCancel && (options.nzOnCancel = onCancel);
+    const options: ModalInterface = ModalConfirmConfig;
+    options.nzTitle = title;
+    options.nzContent = content;
+    onOk && (options.nzOnOk = onOk);
+    onCancel && (options.nzOnCancel = onCancel);
 
-        switch (type) {
-            case TypeModalEnum.CONFIRM:
-                modal = this.nzModalService.confirm(options);
-                break;
-            case TypeModalEnum.INFO:
-                modal = this.nzModalService.info(options);
-                break;
-            case TypeModalEnum.SUCCESS:
-                modal = this.nzModalService.success(options);
-                break;
-            case TypeModalEnum.ERROR:
-                modal = this.nzModalService.error(options);
-                break;
-            case TypeModalEnum.WARNING:
-                modal = this.nzModalService.warning(options);
-                break;
-        }
+    switch (type) {
+      case TypeModalEnum.CONFIRM:
+        modal = this.nzModalService.confirm(options);
+        break;
+      case TypeModalEnum.INFO:
+        modal = this.nzModalService.info(options);
+        break;
+      case TypeModalEnum.SUCCESS:
+        modal = this.nzModalService.success(options);
+        break;
+      case TypeModalEnum.ERROR:
+        modal = this.nzModalService.error(options);
+        break;
+      case TypeModalEnum.WARNING:
+        modal = this.nzModalService.warning(options);
+        break;
+    }
 
-        modal?.afterClose.subscribe(() => modal.destroy());
+    modal?.afterClose.subscribe(() => modal.destroy());
 
-        if (modal && isAutoClose) {
-            setTimeout(() => modal.destroy(), timeAutoClose);
-        }
-    };
+    if (modal && isAutoClose) {
+      setTimeout(() => modal.destroy(), timeAutoClose);
+    }
+  };
 }
